f0740708d0e8bb038537c58249ceff3a3a8121b6
n0rmanc <s965202030@gmail.com>
2026-08-01 20:42:14 +0800

    chore: update Homebrew formula for v0.1.7

--- Formula/longrun.rb
+++ Formula/longrun.rb
@@ -5,20 +5,20 @@ class Longrun < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/n0rmanc/longrun/releases/download/v0.1.4/longrun-aarch64-apple-darwin.tar.gz"
-      sha256 "8867e05a81402f7189d1f7d275612960b8bc42fde78b7b32bd1b9a70c4a259b6"
+      url "https://github.com/n0rmanc/longrun/releases/download/v0.1.7/longrun-aarch64-apple-darwin.tar.gz"
+      sha256 "e6eb70cdeccad402c9c5c8c1cb7c5444fd08d8aaf7cbe970028c8c009b42d5bb"
     end
 
     on_intel do
-      url "https://github.com/n0rmanc/longrun/releases/download/v0.1.4/longrun-x86_64-apple-darwin.tar.gz"
-      sha256 "780681645c4ff5d9c38456659b2270e75f8b719f70e5ef66b6c4c190ba9ad575"
+      url "https://github.com/n0rmanc/longrun/releases/download/v0.1.7/longrun-x86_64-apple-darwin.tar.gz"
+      sha256 "efb3c284f60761ad826bc38e3a9cb3a445e840aa0efb2a7feeb66fb099e4b23a"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/n0rmanc/longrun/releases/download/v0.1.4/longrun-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "f04c7afaf2dd91690765027bd45a449096cf2f0f3f55e52b4a0194ca05162a18"
+      url "https://github.com/n0rmanc/longrun/releases/download/v0.1.7/longrun-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "a487a1190b0f8a69747639a0c81ce208c34ad38be1d7ec751472c5196314402c"
     end
   end
 
